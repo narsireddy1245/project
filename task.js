5920f72a55412dd46ebd910be1b2a75bb1f3d096
janardhan <samalajan2@gmail.com>
2023-07-17 17:02:04 +0530

    code

---
+++ task.js
@@ -0,0 +1,23 @@
+function taskButton(){
+    document.getElementById("fullname").textContent="fullname";
+    document.getElementById("gender").style.display = "";
+    document.getElementById("image").src="https://img.freepik.com/premium-vector/online-registration-sign-up-with-man-sitting-near-smartphone_268404-95.jpg";
+    document.getElementById("icon2").style.accentColor="white";
+    document.getElementById("icon1").style.accentColor="blue";
+    document.getElementById("taskButton2").style.color="lightgray";
+    document.getElementById("taskButton").style.color="";
+
+ }
+
+ function taskButton2(){
+    document.getElementById("fullname").textContent="company name";
+    document.getElementById("gender").style.display ="none"
+    document.getElementById("image").src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg?w=2000";
+    document.getElementById("icon1").style.accentColor="white";
+    document.getElementById("icon2").style.accentColor="blue";
+    document.getElementById("taskButton").style.color="lightgray";
+    document.getElementById("taskButton2").style.color="";
+    document.getElementById("taskButton2").style.fontWeight="bold";
+   
+
+ }
